779c6c31bbc4b0bd34e34dbabc420aaac8958cbd
orkeilius <63045634+orkeilius@users.noreply.github.com>
2026-07-21 08:15:28 +0200

    fix (window-system): reisizing to top/left didn't work

--- src/components/WindowsManager.tsx
+++ src/components/WindowsManager.tsx
@@ -1,10 +1,9 @@
-import {useEffect, useRef, useState} from "react";
+import {SyntheticEvent, useEffect, useRef, useState} from "react";
 import Draggable from "react-draggable";
-import {ResizableBox} from "react-resizable";
+import {ResizableBox, ResizeCallbackData} from "react-resizable";
 import {useWindows, type WindowConfig} from "../context/WindowsContext";
 import "../css/WindowManager.css";
 import MainPage from "../pages/MainPage";
-
 
 export default function WindowsManager() {
     const windowsManger = useWindows();
@@ -32,7 +31,8 @@ export default function WindowsManager() {
     );
 }
 
-function WindowInstance({config,onClose,
+function WindowInstance({
+                            config, onClose,
                         }: {
     config: WindowConfig;
     onClose: () => void;
@@ -43,24 +43,39 @@ function WindowInstance({config,onClose,
     const isClosable = config.isClosable ?? true;
     const minW = config.minSize?.width ?? 200;
     const minH = config.minSize?.height ?? 80;
-    const size = config.defaultSize ?? {width: 400, height: 300};
+    const [size,setSize] = useState(config.defaultSize ?? {width: 400, height: 300});
+
+    const correctResizePos = (event: SyntheticEvent, newSize: ResizeCallbackData) => {
+        if (!newSize.handle.includes("n") && !newSize.handle.includes("w")) {
+            return
+        }
+        setPos(
+            {
+                x: pos.x - (newSize.size.width - size.width),
+                y: pos.y - (newSize.size.height - size.height)
+            }
+        )
+        setSize(newSize.size)
+    }
 
     const nodeRef = useRef(null)
     return (
         <Draggable
-            //handle=".win-titlebar"
             position={pos}
             nodeRef={nodeRef}
             handle=".window-titlebar"
             onDrag={(_, data) => setPos({x: data.x, y: data.y})}
         >
-            <div ref={nodeRef}>
+            <div
+                ref={nodeRef}>
                 <ResizableBox
                     width={size.width}
                     height={size.height}
                     minConstraints={[minW, minH]}
+                    onResize={correctResizePos}
                     resizeHandles={["se", "sw", "ne", "nw", "e", "w", "n", "s"]}
-                    handle={(handleAxis, ref) => <div ref={ref} className={"window-handle window-handle-"+ handleAxis} /> }
+                    handle={(handleAxis, ref) => <div ref={ref}
+                                                      className={"window-handle window-handle-" + handleAxis}/>}
                 >
                     <div
                         className="window-card">
@@ -76,7 +91,8 @@ function WindowInstance({config,onClose,
                             <config.content/>
                         </div>
                     </div>
-                </ResizableBox></div>
+                </ResizableBox>
+            </div>
         </Draggable>
     );
 }
