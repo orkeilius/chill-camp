d114438384e711dd8cf029b872649616629fff10
orkeilius <63045634+orkeilius@users.noreply.github.com>
2026-07-22 08:26:34 +0200

    refactor (window-system): split windows manger/instance

---
+++ src/components/WindowInstance.tsx
@@ -0,0 +1,72 @@
+import {SyntheticEvent, useRef, useState} from "react";
+import {ResizableBox, ResizeCallbackData} from "react-resizable";
+import Draggable from "react-draggable";
+import type {WindowConfig} from "../context/WindowsContext";
+
+type WindowIntanceProps = {
+    config: WindowConfig;
+    onClose: () => void;
+}
+
+export function WindowInstance({
+                                   config, onClose,
+                               }: Readonly<WindowIntanceProps>) {
+    const [pos, setPos] = useState(
+        config.defaultPosition ?? {x: 50, y: 50},
+    );
+    const isClosable = config.isClosable ?? true;
+    const minW = config.minSize?.width ?? 200;
+    const minH = config.minSize?.height ?? 80;
+    const [size, setSize] = useState(config.defaultSize ?? {width: 400, height: 300});
+
+    const correctResizePos = (event: SyntheticEvent, newSize: ResizeCallbackData) => {
+        if (!newSize.handle.includes("n") && !newSize.handle.includes("w")) {
+            return
+        }
+        setPos(
+            {
+                x: pos.x - (newSize.size.width - size.width),
+                y: pos.y - (newSize.size.height - size.height)
+            }
+        )
+        setSize(newSize.size)
+    }
+
+    const nodeRef = useRef(null)
+    return (
+        <Draggable
+            position={pos}
+            nodeRef={nodeRef}
+            handle=".window-titlebar"
+            onDrag={(_, data) => setPos({x: data.x, y: data.y})}
+        >
+            <div
+                ref={nodeRef}>
+                <ResizableBox
+                    width={size.width}
+                    height={size.height}
+                    minConstraints={[minW, minH]}
+                    onResize={correctResizePos}
+                    resizeHandles={["se", "sw", "ne", "nw", "e", "w", "n", "s"]}
+                    handle={(handleAxis, ref) => <div ref={ref}
+                                                      className={"window-handle window-handle-" + handleAxis}/>}
+                >
+                    <div
+                        className="window-card">
+                        <div className="window-titlebar">
+                            <span className="wm-titlebar-text">{config.title}</span>
+                            {isClosable && (
+                                <button onClick={onClose} className="wm-close-btn">
+                                    ✕
+                                </button>
+                            )}
+                        </div>
+                        <div className="wm-content">
+                            <config.content/>
+                        </div>
+                    </div>
+                </ResizableBox>
+            </div>
+        </Draggable>
+    );
+}
--- src/components/WindowsManager.tsx
+++ src/components/WindowsManager.tsx
@@ -1,9 +1,8 @@
-import {SyntheticEvent, useEffect, useRef, useState} from "react";
-import Draggable from "react-draggable";
-import {ResizableBox, ResizeCallbackData} from "react-resizable";
-import {useWindows, type WindowConfig} from "../context/WindowsContext";
+import {useEffect} from "react";
+import {useWindows} from "../context/WindowsContext";
 import "../css/WindowManager.css";
 import MainPage from "../pages/MainPage";
+import {WindowInstance} from "./WindowInstance";
 
 export default function WindowsManager() {
     const windowsManger = useWindows();
@@ -31,68 +30,3 @@ export default function WindowsManager() {
     );
 }
 
-function WindowInstance({
-                            config, onClose,
-                        }: {
-    config: WindowConfig;
-    onClose: () => void;
-}) {
-    const [pos, setPos] = useState(
-        config.defaultPosition ?? {x: 50, y: 50},
-    );
-    const isClosable = config.isClosable ?? true;
-    const minW = config.minSize?.width ?? 200;
-    const minH = config.minSize?.height ?? 80;
-    const [size,setSize] = useState(config.defaultSize ?? {width: 400, height: 300});
-
-    const correctResizePos = (event: SyntheticEvent, newSize: ResizeCallbackData) => {
-        if (!newSize.handle.includes("n") && !newSize.handle.includes("w")) {
-            return
-        }
-        setPos(
-            {
-                x: pos.x - (newSize.size.width - size.width),
-                y: pos.y - (newSize.size.height - size.height)
-            }
-        )
-        setSize(newSize.size)
-    }
-
-    const nodeRef = useRef(null)
-    return (
-        <Draggable
-            position={pos}
-            nodeRef={nodeRef}
-            handle=".window-titlebar"
-            onDrag={(_, data) => setPos({x: data.x, y: data.y})}
-        >
-            <div
-                ref={nodeRef}>
-                <ResizableBox
-                    width={size.width}
-                    height={size.height}
-                    minConstraints={[minW, minH]}
-                    onResize={correctResizePos}
-                    resizeHandles={["se", "sw", "ne", "nw", "e", "w", "n", "s"]}
-                    handle={(handleAxis, ref) => <div ref={ref}
-                                                      className={"window-handle window-handle-" + handleAxis}/>}
-                >
-                    <div
-                        className="window-card">
-                        <div className="window-titlebar">
-                            <span className="wm-titlebar-text">{config.title}</span>
-                            {isClosable && (
-                                <button onClick={onClose} className="wm-close-btn">
-                                    ✕
-                                </button>
-                            )}
-                        </div>
-                        <div className="wm-content">
-                            <config.content/>
-                        </div>
-                    </div>
-                </ResizableBox>
-            </div>
-        </Draggable>
-    );
-}
